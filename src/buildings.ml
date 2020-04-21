88bdab7d06a99a565945bcceba01e82c31a3864b
irisval <iris.val19@gmail.com>
2020-04-20 21:13:45 -0400

    Added data files.

--- src/buildings.ml
+++ src/buildings.ml
@@ -1,20 +1,24 @@
 type resources
 type coordinates = int*int
 
-module BuildingType = struct
-  type t = {
-    name: string;
-    cost : resources;
-    output : resources;
-  }
-end
+type building_type = {
+  name: string;
+  cost : resources;
+  output : resources;
+}
 
 type building_type_id = int
 
-module Building = struct
-  type t = {
+(* module Building = struct
+   type t = {
     type_id: building_type_id;
     position: coordinates;
     worker_count: int;
-  }
-end
+   }
+   end *)
+
+type building = {
+  type_id: building_type_id;
+  position: coordinates;
+  worker_count: int;
+}
--- src/gamestate.mli
+++ src/gamestate.mli
@@ -1,9 +1,11 @@
 type coordinates
 type building_type_id
 type building_id
+type building_type
 type building
 type tile_type
 
+(** The type representing a game state *)
 type t
 
 val init_state : t
@@ -12,15 +14,17 @@ val step : t -> t
 
 val can_place_building : building_type_id -> coordinates -> t -> bool
 
-val place_building : building_type_id -> coordinates -> t -> t
+val place_building : building_type_id -> coordinates -> int -> t -> t
 
-val set_workers : building_id -> t -> t
+val set_workers : building_id -> int -> t -> t
 
 val alive : t -> bool
 
 val get_resources : t -> (string*int) list
 
-val get_buildings : t -> building
+val get_building_types : t -> (building_type_id*building_type) list
+
+val get_buildings : t -> building list
 
 val get_building_at : coordinates -> t -> building option
 
